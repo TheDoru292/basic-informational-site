dbf210bcb9c8478897f2189cf24c096ae356e558
thedoru <thedoruv2@gmail.com>
2022-11-29 18:33:37 +0100

    Express version of the file

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const port = 80;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(port);
